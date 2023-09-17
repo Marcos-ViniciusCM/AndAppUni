04f70dd1a9346f26e612f8359d3fc331b9baa1b9
Pedra-910 <MarcosViniciusCM@outlook.com>
2023-09-17 10:25:22 -0300

    criarBancoDados
    criei o banco de dados,e comecei a parte de alteraçao dele

--- app/src/main/java/com/example/anjosgaar/MainActivity.java
+++ app/src/main/java/com/example/anjosgaar/MainActivity.java
@@ -2,15 +2,36 @@ package com.example.anjosgaar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private SQLiteDatabase bancoDados;
+   // criar o list view de add no banco public ListView listViewDados;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);]
+        
+        criarBancoDados();
+        listarDados();
         
     }
+     public void criarBancoDados(){
+        try{
+            bancoDados= openOrCreateDatabase("anjos",MODE_PRIVATE,null);//(nome do banco , private,  e null)
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS  coisas("+
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                    ",nome VARCHAR)");
+            bancoDados.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+     }
+     public void listarDados(){
+
+     }
 
 }
