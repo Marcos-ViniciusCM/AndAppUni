a717abf36c6dbd90f1d671b37f01435ba11215ad
LuanAur <127259655+LuanAur@users.noreply.github.com>
2023-09-20 17:59:23 -0300

    Logo Test+Splash

--- app/src/main/java/com/example/appanjos/MainActivity.java
+++ app/src/main/java/com/example/appanjos/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appanjos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.os.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent=new Intent (MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
     }
 }
