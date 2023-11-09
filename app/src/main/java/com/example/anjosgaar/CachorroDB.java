04f9fd9179c52fbf793691b6be4da985a0d0bd80
Pedra-910 <MarcosViniciusCM@outlook.com>
2023-11-08 21:01:12 -0300

    add Delete

--- app/src/main/java/com/example/anjosgaar/CachorroDB.java
+++ app/src/main/java/com/example/anjosgaar/CachorroDB.java
@@ -20,7 +20,7 @@ public class CachorroDB {
             stmt.setString(1,nome);
             stmt.setString(2,sexo);
             stmt.setString(3,descricao);
-           stmt.executeUpdate();
+            stmt.executeUpdate();
             Log.e("TAG", "adicionouuuuuu");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -39,8 +39,41 @@ public class CachorroDB {
         }
     }
 
+    public static void delete(int id){
+        Connection con = null;
+        PreparedStatement stmt = null;
+        try{
+            con = ConexaoFactory.getConexao();
+            String sql = "DELETE FROM cachorros WHERE id = ?";
+            stmt = con.prepareStatement(sql);
+            stmt.setInt(1,id);
+            int colunasAfetadas = stmt.executeUpdate();
+            if(colunasAfetadas > 0){
+                Log.e("exclusao", "registro excluido");
+            }else{
+                Log.e("erro_exclusao", "nenhum registro excluido");
+            }
 
-       }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            try{
+                if(stmt != null){
+                    stmt.close();
+                }
+                if(con != null){
+                    con.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+      }
+
+
+
+}
 
 
 
